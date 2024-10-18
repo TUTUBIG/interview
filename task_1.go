9a500f1363ccf68defaf99ca44eee9928685745f
alvinliu <kui@ankr.com>
2024-10-18 15:41:56 +0800

    task

--- task_1.go
+++ task_1.go
@@ -1,1 +1,28 @@
-package interview
+package main
+
+import (
+	"fmt"
+	"os"
+	"os/signal"
+	"time"
+)
+
+func main() {
+	var shutdown chan os.Signal
+	signal.Notify(shutdown, os.Interrupt)
+
+	for i := 0; i < 3; i++ {
+		go func() {}()
+	}
+
+	fmt.Println("done")
+
+	return
+}
+
+func job(i int) {
+	now := time.Now().Unix()
+	fmt.Println("job start", i, now)
+	time.Sleep(5 * time.Second)
+	fmt.Println("job end", i, now)
+}
